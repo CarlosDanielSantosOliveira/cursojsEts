dfc9a3c20d88d5a80e220e6c397f0201b0ab5298
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-01-31 12:17:28 -0300

    Criação de models e configuração do arquivo env

--- seção 10 node Express e MongoDb/MongoDb - conexão e primeiro model/server.js
+++ seção 10 node Express e MongoDb/MongoDb - conexão e primeiro model/server.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+//Conectando com a base de dados
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Conectei à base de dados.')
+        app.emit('pronto') //Dessa forma, assim que a aplicação se conetar com o banco de dados, vai emitir uma mensagem de pronto
+    })
+    .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
@@ -18,7 +31,12 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use(routes);
 app.use(middlewareGlobal);
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
-});
+
+app.on('pronto', () => { //Quando a conexão do banco de dados estiver pronta, vamos executar uma função
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
+})
+
+ 

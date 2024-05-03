b76c8d1de21027bef0e82cea1fe901d7c6d9e27f
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-05-02 22:23:51 -0300

    FUncionalidade de remover e editar

--- modulo 14 - React com componentes de classe (básico)/Lista de tarefas/listadetarefas/src/components/Main1.js
+++ modulo 14 - React com componentes de classe (básico)/Lista de tarefas/listadetarefas/src/components/Main1.js
@@ -2,7 +2,8 @@
 //Ele vai possuir estado, logo, é uma classe
 import React, { Component } from 'react';
 import './Main.css';
-import {FaPlus} from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 //Nesta classe nós estamos criando estado pela primeira forma.
 export default class Main extends Component {
@@ -14,9 +15,37 @@ export default class Main extends Component {
         this.state = {
             //Inicializei o estado
             novaTarefa: '',
+            tarefas: [],
+            index: -1
         }
 
         this.inputMudou = this.inputMudou.bind(this);
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { tarefas, index } = this.state;
+        let { novaTarefa } = this.state;
+        novaTarefa = novaTarefa.trim();
+
+        if (tarefas.indexOf(novaTarefa) != -1) return;
+
+        const novasTarefas = [...tarefas];
+
+        if (index == -1) {
+            this.setState({
+                tarefas: [...novasTarefas, novaTarefa],
+                novaTarefa: '',
+            });
+        } else {
+            novasTarefas[index] = novaTarefa;
+
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            })
+        }
+
     }
 
     inputMudou(e) {
@@ -26,25 +55,58 @@ export default class Main extends Component {
             novaTarefa: e.target.value, //Dessa forma nós vamos capturar os eventos dentro do input e atualizar o estado;
         });
     }
+
+    handleEdit = (e, index) => {
+        const { tarefas } = this.state;
+
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        });
+    }
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state;
+        const novasTarefas = [...tarefas];
+        novasTarefas.splice(index, 1);
+
+        this.setState({
+            tarefas: [...novasTarefas],
+        })
+    }
+
     render() {
 
-        const { novaTarefa } = this.state;
+        const { novaTarefa, tarefas } = this.state;
 
         return (
             <div className='main'>
                 <h1> Lista de tarefas </h1>
                 <h1> {novaTarefa} </h1>
 
-                <form action="#" className="form"> 
-                    <input onChange={this.inputMudou} 
-                    type="text" 
-                    value={novaTarefa}
-                    /> 
+                <form onSubmit={this.handleSubmit} action="#" className="form">
+                    <input onChange={this.inputMudou}
+                        type="text"
+                        value={novaTarefa}
+                    />
                     <button type="submit">
                         <FaPlus />
                     </button>
 
                 </form>
+
+                {/* Aqui nós vamos codar em javascript dentro do ul para mapear as tarefas da lista a cada volta do for */}
+                <ul className="tarefas">
+                    {tarefas.map((tarefa, index) => (
+                        <li key={tarefa}>
+                            {tarefa}
+                            <span>
+                                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
+                                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
+                            </span>
+                        </li> //Deixamos a propria tarefa como a chave do elemento
+                    ))}
+                </ul>
             </div>
         )
     }

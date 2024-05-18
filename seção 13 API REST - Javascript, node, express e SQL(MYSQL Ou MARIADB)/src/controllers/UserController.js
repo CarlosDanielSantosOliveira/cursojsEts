7c4c8d714a032b202d41ceccccc248593e230571
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-05-18 11:47:07 -0300

    Criando seeds, os seeds criam um arquivo que podemos setar dados mockados em uma tabela e vamos utilizar isso para criar uma base de dados de alunos

--- seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/src/controllers/UserController.js
+++ seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/src/controllers/UserController.js
@@ -5,7 +5,8 @@ class UserController {
 
         try {
             const novoUser = await User.create(req.body);
-            return res.json(novoUser);
+            const { id, nome, email } = novoUser;
+            return res.json({ id, nome, email });
         } catch (e) {
             return res.status(400).json({
                 errors: e.errors.map((err) => err.message),
@@ -16,16 +17,20 @@ class UserController {
     //Index
     async index(req, res) {
         try {
-            const users = await User.findAll();
+            const users = await User.findAll({ attributes: ['id', 'nome', 'email']});
+            
             return res.json(users);
         } catch(e) {
             return res.json(null);
         }
     }
+
     //Show
     async show(req, res) {
         try {
             const user = await User.findByPk(req.params.id);
+
+            const { id, nome, email } = user;
             return res.json(user);
         } catch(e) {
             return res.json(null);
@@ -35,14 +40,7 @@ class UserController {
     //Update
     async update(req, res) {
         try {
- 
-            if(!req.params.id) {
-                return res.status(400).json({
-                    errors: ['ID não enviado']
-                })
-            }
-
-            const user = await User.findByPk(req.params.id);
+            const user = await User.findByPk(req.userId);
             
             if(!user) {
                 return res.status(400).json({
@@ -51,8 +49,9 @@ class UserController {
             }
             
             const novosDados = await user.update(req.body);
-            
-            return res.json(novosDados);
+            const { id, nome, email } = novosDados;
+
+            return res.json({ id, nome, email });
         } catch(e) {
             return res.status(400).json({
                 errors: e.errors.map((err) => err.message),
@@ -63,13 +62,8 @@ class UserController {
     //Delete
      async delete(req, res) {
         try {
-            if(!req.params.id) {
-                return res.status(400).json({
-                    errors: ['ID não enviado']
-                })
-            }
-
-            const user = await User.findByPk(req.params.id);
+            
+            const user = await User.findByPk(req.userId);
             
             if(!user) {
                 return res.status(400).json({
@@ -79,7 +73,7 @@ class UserController {
             
             await user.destroy();
             
-            return res.json(user);
+            return res.json(null);
         } catch(e) {
             return res.status(400).json({
                 errors: e.errors.map((err) => err.message),

62a4432f48971e2ea4b70056005aa41dcffc7ea1
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-05-26 12:55:05 -0300

    CRUD alunos

--- seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/src/controllers/AlunoController.js
+++ seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/src/controllers/AlunoController.js
@@ -7,6 +7,104 @@ class AlunoController {
 
         res.json(alunos);
     }
+
+    async store(req, res) {
+        try {
+            const aluno = await Aluno.create(req.body);
+
+            return res.json(aluno);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            })
+        }
+    }
+
+    async show(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const aluno = await Aluno.findByPk(id);
+
+            if(!aluno) {
+                return res.status(400).json({
+                    errors: ['Aluno não existe'],
+                })
+            }
+
+            return res.json(aluno);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+        
+    }
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const aluno = await Aluno.findByPk(id);
+
+            if(!aluno) {
+                return res.status(400).json({
+                    errors: ['Aluno não existe'],
+                })
+            }
+
+            await aluno.destroy();
+
+            return res.json({
+                apagado: true,
+            });
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const aluno = await Aluno.findByPk(id);
+
+            if(!aluno) {
+                return res.status(400).json({
+                    errors: ['Aluno não existe'],
+                })
+            }
+
+            const alunoAtualizado = await aluno.update(req.body);
+            
+            return res.json(alunoAtualizado);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+        
+    }
 }
 
 export default new AlunoController(); //Dessa forma nós ja exportamos instanciado

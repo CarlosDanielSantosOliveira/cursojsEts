bdc2ad1cc4d62a0149fd155c5d6b1db71b603265
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-05-26 23:48:28 -0300

    Salvando a foto na base de dados

--- seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/app.js
+++ seção 13 API REST - Javascript, node, express e SQL(MYSQL Ou MARIADB)/app.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import './src/database';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/AlunoRoutes';
-import './src/database';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ class App {
         this.app.use('/users/', userRoutes);
         this.app.use('/tokens/', tokenRoutes);
         this.app.use('/alunos/', alunoRoutes);
+        this.app.use('/fotos/', fotoRoutes);
     }
 }
 

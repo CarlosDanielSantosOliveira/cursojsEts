4a1b57ee0e50e6a956e5d1b65d711feea96693c7
CarlosDanielSantosOliveira <carlos_daniel_921@outlook.com>
2024-05-28 23:50:57 -0300

    Styled components

---
+++ seção 15 - React - Redux - Redux Saga - React Hooks e componentes funcionais/projeto/src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Title } from './styled';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Login() {
+    return (
+        <>
+        <Container>
+            <Title>
+                Login
+                <small> Oie </small>
+            </Title>
+            <p> Lorem ipsum dolor sit amet</p>
+        </Container>
+        </>
+    )
+}
